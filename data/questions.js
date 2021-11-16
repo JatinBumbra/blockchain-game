0a2fc72670fb337205fbdbb4d157db666e71b730
JatinBumbra <jatin.bumbra.23@gmail.com>
2021-11-17 03:01:37 +0530

    Complete frontend UI

---
+++ data/questions.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    question:
+      '1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe tempore labore vel fugit error accusantium inventore, architecto autem consequuntur perferendis.',
+    answer: 0,
+    options: ['lorem', 'ipsum', 'dolar', 'sit'],
+  },
+  {
+    question:
+      '2. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe tempore labore vel fugit error accusantium inventore, architecto autem consequuntur perferendis.',
+    answer: 2,
+    options: ['lorem', 'ipsum', 'dolar', 'sit'],
+  },
+  {
+    question:
+      '3. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe tempore labore vel fugit error accusantium inventore, architecto autem consequuntur perferendis.',
+    answer: 3,
+    options: ['lorem', 'ipsum', 'dolar', 'sit'],
+  },
+  {
+    question:
+      '4. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe tempore labore vel fugit error accusantium inventore, architecto autem consequuntur perferendis.',
+    answer: 0,
+    options: ['lorem', 'ipsum', 'dolar', 'sit'],
+  },
+  {
+    question:
+      '5. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe tempore labore vel fugit error accusantium inventore, architecto autem consequuntur perferendis.',
+    answer: 1,
+    options: ['lorem', 'ipsum', 'dolar', 'sit'],
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,132 @@
-import Head from 'next/head'
+import { useState } from 'react';
+import questions from '../data/questions';
 
 export default function Home() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [activeOption, setActiveOption] = useState();
+  const [answersGiven, setAnswersGiven] = useState([]);
+  const [isQuizCompleted, setIsQuizCompleted] = useState(false);
+
+  const handleNext = (e) => {
+    if (activeQuestionIndex === questions.length - 1)
+      return setIsQuizCompleted(true);
+    setActiveQuestionIndex((prev) => prev + 1);
+    setActiveOption();
+  };
+
+  const handleSelected = (e) => {
+    if (activeOption) return;
+    setActiveOption(e.target.id);
+    setAnswersGiven((prev) => [...prev, e.target.id]);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+    <main className='h-screen overflow-hidden'>
+      {/* Grid */}
+      <div className='grid grid-cols-2'>
+        {/* Hero Section */}
+        <div className='p-10 pr-20'>
+          {/* Header */}
+          <header className='py-3 flex justify-between'>
+            <p className='text-2xl font-medium'>
+              <span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-indigo-500 font-black'>
+                Blockchain
+              </span>
+              <span className='text-gray-400'> Quiz</span>
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </header>
+          <h1 className='text-8xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-indigo-500 font-black mt-6'>
+            Learn and Earn
+          </h1>
+          <h2 className='text-xl text-gray-400 font-semibold mt-3 mb-20'>
+            Test your knowledge on Blockchain Technology and earn KIT tokens for
+            the ones you know.
+          </h2>
+          <p className='text-xl font-semibold text-gray-700'>KIT Earned</p>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+        {/* Questions */}
+        <div className='bg-gradient-to-r from-blue-400 to-indigo-500 h-screen overflow-y-scroll'>
+          <div className='m-10 bg-white rounded-2xl p-5 shadow-2xl'>
+            {isQuizCompleted ? (
+              <div>
+                <h3 className='text-3xl font-bold text-gray-900 mb-4 mt-2'>
+                  Here are your Results
+                </h3>
+                {questions.map((qt, i) => (
+                  <div className='mb-8' key={i}>
+                    <p className='text-gray-500'>{qt.question}</p>
+                    <div className='mt-2'>
+                      <p className='text-gray-900 font-semibold text-xs mb-1 ml-1'>
+                        Correct
+                      </p>
+                      <div className='border px-4 py-3 rounded-md bg-green-100 border-green-500'>
+                        {qt.options[qt.answer]}
+                      </div>
+                    </div>
+                    <div className='mt-3'>
+                      <p className='text-gray-900 font-semibold text-xs mb-1 ml-1'>
+                        Your Answer
+                      </p>
+                      <div
+                        className={`border px-4 py-3 rounded-md bg-${
+                          qt.answer == answersGiven[i] ? 'green' : 'red'
+                        }-100 border-${
+                          qt.answer == answersGiven[i] ? 'green' : 'red'
+                        }-500`}
+                      >
+                        {qt.options[answersGiven[i]]}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                <p className='text-2xl font-semibold text-gray-900'>
+                  Your Rewards: {}
+                </p>
+                <button className='text-white bg-indigo-500 py-2 px-6 rounded-md hover:bg-indigo-600 active:bg-indigo-700 justify-self-center w-full mt-4 disabled:cursor-not-allowed disabled:opacity-50'>
+                  Get Rewards
+                </button>
+              </div>
+            ) : (
+              <div>
+                <p className='text-gray-400 font-medium mb-3'>
+                  Question {activeQuestionIndex + 1} / 5
+                </p>
+                <h2 className='text-lg text-gray-900 font-medium mb-4'>
+                  {questions[activeQuestionIndex].question}
+                </h2>
+                {questions[activeQuestionIndex].options.map((op, i) => (
+                  <div
+                    id={i}
+                    className={`border px-4 py-3 my-3 cursor-pointer rounded-md ${
+                      activeOption && i == questions[activeQuestionIndex].answer
+                        ? 'bg-green-100 border-green-500'
+                        : ''
+                    } ${
+                      activeOption != questions[activeQuestionIndex].answer &&
+                      i == activeOption
+                        ? 'bg-red-100 border-red-500'
+                        : ''
+                    }`}
+                    key={op}
+                    onClick={handleSelected}
+                  >
+                    {op}
+                  </div>
+                ))}
+                <button
+                  className='text-white bg-indigo-500 py-2 px-6 rounded-md hover:bg-indigo-600 active:bg-indigo-700 justify-self-center w-full mt-4 disabled:cursor-not-allowed disabled:opacity-50'
+                  onClick={handleNext}
+                  disabled={activeOption === undefined}
+                >
+                  {activeQuestionIndex === questions.length - 1
+                    ? 'View Results'
+                    : 'Next'}
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
